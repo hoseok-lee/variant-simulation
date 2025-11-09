ce1b455312381a40b329f7c8c4436073265b6132
Ho Seok Lee <hoseoklee99@proton.me>
2025-11-09 10:55:55 -0500

    Solved problem, updated README and simulate.py

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Problem Formulation
 
-> Given a table of $n$ variants $v_i$ with associated minor allele frequencies (MAF) $P(v_i)$, is it possible to model the distribution of the number of variants any certain individual is expected to carry?
+> Given a table of $n$ variants $v_i$ with associated minor allele frequencies (MAF) $P(v_i) = p_i$, is it possible to model the distribution of the number of variants any certain individual is expected to carry?
 
 # Solution
 
@@ -9,7 +9,19 @@ iterating through the entire list of variants, sampling a uniform distribution
 for each variant to determine whether the variant is present in the individual,
 and creating histogram of the total sum of variants.
 
-TODO: Solve it!
+Considering each variant $X_i$ to be binomial distribution of exactly one test
+(i.e. does the patient exhibit this variant or no?) with probability $p_i$ of
+success, the mean and variance of $X_i$ is $\mu_i = p_i$ and
+$\sigma_i^2 = p_i (1 - p_i)$. Further considering each of these random variables
+to be independent, the sum of all variants $X_i$ will have a mean and variance
+of
+
+$$\mu = \sum_i \mu_i = \sum_i p_i$$
+$$\sigma^2 = \sum_i \sigma_i = \sum_i p_i (1 - p_i)$$
+
+Due to the Central Limit Theorem (CTL), the total number of variants any
+individual carries is normally distributed with mean $\mu$ and variance
+$\sigma^2$.
 
 # Usage
 
--- simulate.py
+++ simulate.py
@@ -1,8 +1,103 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
+from scipy.stats import norm, kstest
 
 
-variants = pd.read_csv("variants.tsv")
+def plot_dist_with_norm(
+    dist: np.ndarray,
+    mu: float,
+    std: float,
+    figure: str
+):
+
+    x = np.linspace(150, 250, 5000)
+    y = norm.pdf(
+        x,
+        loc = mu,
+        scale = std
+    )
+
+    plt.clf()
+    plt.hist(
+        dist,
+        bins = 20,
+        density = True,
+        label = "Experimental"
+    )
+    plt.plot(x, y, label = "Theoretical")
+    plt.legend()
+    plt.xlabel("Total Number of Variants")
+    plt.ylabel("Probability Density")
+    plt.savefig(figure)
 
 
-# def one_sample_ground_truth(variants):
+def single_bernoulli(mafs):
+    ber_test = np.random.rand(mafs.shape[0])
+    return (mafs['maf'].values > ber_test).sum()
+
+def bernoulli_test(
+    mafs: pd.DataFrame,
+    n_patients: int
+):
+
+    np.random.seed(0)
+
+    dist = np.array([
+        single_bernoulli(mafs)
+        for _ in range(n_patients)
+    ])
+
+    mu, std = norm.fit(dist)
+
+    plot_dist_with_norm(dist, mu, std, figure = "bernoulli.png")
+    res, p = kstest((dist - mu) / std, "norm")
+
+    return mu, std, res, p, dist
+
+
+def normal_test(
+    mafs: pd.DataFrame,
+    n_patients: int,
+    bernoulli: np.ndarray
+):
+
+    # Compute with hypothesized
+    mu = mafs['maf'].sum()
+    std = np.sqrt((mafs['maf'] * (1 - mafs['maf'])).sum())
+
+    dist = norm.rvs(
+        loc = mu,
+        scale = std,
+        size = n_patients
+    )
+
+    plot_dist_with_norm(dist, mu, std, figure = "normal.png")
+    res, p = kstest(dist, bernoulli)
+
+    return mu, std, res, p, dist
+
+
+mafs = pd.read_csv(
+    "mafs.tsv",
+    delimiter = "\t",
+    index_col = 0
+)
+
+n_patients = 50000
+
+mu_b, std_b, res_b, p_b, dist_b = bernoulli_test(mafs, n_patients)
+mu_n, std_n, res_n, p_n, dist_n = normal_test(mafs, n_patients, dist_b)
+
+df = pd.DataFrame(
+    {
+        'mean':     [mu_b, mu_n],
+        'std':      [std_b, std_n],
+        'ks_test':  [res_b, res_n],
+        'pvalue':   [p_b, p_n]
+    },
+    index = ["bernoulli", "normal"]
+)
+
+print(df)
